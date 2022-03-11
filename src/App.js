be808c70795380ace1c18a458cfe2af4bdf81c5a
Vuyani Mampofu <Vuyani@bluegrassdigital.com>
2022-03-11 12:00:54 +0200

    Changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Translate from "./components/Translate";
 const items =[
     {
         title: 'What is React?',
-        content: 'React is a front end javascript framework'
+        content: 'React is a front end javascript framework. I love it.'
     },
     {
         title: 'Why use React?',
